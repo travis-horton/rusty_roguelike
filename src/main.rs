91d161778b5811290347de1d9d57066c2d18945f
kiddspazz <travis@travish.com>
2022-02-12 13:06:45 -0700

    Implement move NPCs

--- src/main.rs
+++ src/main.rs
@@ -23,12 +23,39 @@ struct State {
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
+
+        self.run_systems();
+
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
 
         for (pos, render) in (&positions, &renderables).join() {
             ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
         }
+
+    }
+}
+
+#[derive(Component)]
+struct LeftWalker {}
+
+impl<'a> System<'a> for LeftWalker {
+    type SystemData = (ReadStorage<'a, LeftWalker>,
+                        WriteStorage<'a, Position>);
+
+    fn run(&mut self, (lefty, mut pos): Self::SystemData) {
+        for (_lefty, pos) in (&lefty, &mut pos).join() {
+            pos.x -= 1;
+            if pos.x < 0 { pos.x = 79; }
+        }
+    }
+}
+
+impl State {
+    fn run_systems(&mut self) {
+        let mut lw = LeftWalker{};
+        lw.run_now(&self.ecs);
+        self.ecs.maintain();
     }
 }
 
@@ -42,6 +69,7 @@ fn main() -> rltk::BError {
     };
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
+    gs.ecs.register::<LeftWalker>();
 
     gs.ecs
         .create_entity()
@@ -51,7 +79,7 @@ fn main() -> rltk::BError {
             fg: RGB::named(rltk::YELLOW),
             bg: RGB::named(rltk::BLACK),
         })
-    .build();
+        .build();
 
     for i in 0..10 {
         gs.ecs
@@ -62,8 +90,9 @@ fn main() -> rltk::BError {
                 fg: RGB::named(rltk::RED),
                 bg: RGB::named(rltk::BLACK),
             })
-        .build();
-    }
+            .with(LeftWalker{})
+            .build();
+        }
 
     rltk::main_loop(context, gs)
 }
